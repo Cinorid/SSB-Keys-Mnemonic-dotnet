17632b5c377a0ce09105e3a9e2c162d25b2d1567
Cinorid <cinorid@gmail.com>
2021-04-19 18:09:17 +0430

    add SSB.Keys.Mnemonic.CLI but nothing implemented

---
+++ SSB.Keys.Mnemonic.CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using SSB.Keys;
+
+namespace SSB.Keys.Mnemonic.CLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length < 1)
+			{
+				Console.WriteLine("use -h or -help for help.");
+			}
+
+		}
+	}
+}
